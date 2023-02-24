7535573a04e5efe9a8969cdef717e18b85c2908b
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-02-24 17:07:15 +0100

    upd 2.0

--- pyroll/zouhar_contact.py
+++ pyroll/zouhar_contact.py
@@ -1,7 +1,6 @@
 from pyroll.core import RollPass, Hook
-from logging import getLogger
 
-VERSION = "2.0.0b"
+VERSION = "2.0"
 
 RollPass.zouhar_contact_c1 = Hook[float]()
 """Get the value of the Zouhar C1 constant for the given roll pass."""
@@ -61,59 +60,59 @@ def contact_area(self: RollPass.Roll):
 
 @RollPass.zouhar_contact_c2
 def diamond_diamond_c2(self: RollPass):
-    if "diamond" in self.in_profile.types and "diamond" in self.types:
+    if "diamond" in self.in_profile.classifiers and "diamond" in self.classifiers:
         return 0.3
 
 
 @RollPass.zouhar_contact_c2
 def diamond_square_c2(self: RollPass):
-    if "diamond" in self.in_profile.types and "square" in self.types:
+    if "diamond" in self.in_profile.classifiers and "square" in self.classifiers:
         return 0.28
 
 
 @RollPass.zouhar_contact_in_width
 def diamond_square_in_width(self: RollPass):
-    if "diamond" in self.in_profile.types and "square" in self.types:
+    if "diamond" in self.in_profile.classifiers and "square" in self.classifiers:
         return self.in_profile.local_width(self.in_profile.height / 2)
 
 
 @RollPass.zouhar_contact_c2
 def oval_square_c2(self: RollPass):
-    if "oval" in self.in_profile.types and "square" in self.types:
+    if "oval" in self.in_profile.classifiers and "square" in self.classifiers:
         return 0.1
 
 
 @RollPass.zouhar_contact_in_width
 def oval_square_in_width(self: RollPass):
-    if "oval" in self.in_profile.types and "square" in self.types:
+    if "oval" in self.in_profile.classifiers and "square" in self.classifiers:
         return self.in_profile.local_width(self.in_profile.height / 2)
 
 
 @RollPass.zouhar_contact_c1
 def round_oval_c1(self: RollPass):
-    if "round" in self.in_profile.types and "oval" in self.types:
+    if "round" in self.in_profile.classifiers and "oval" in self.classifiers:
         return 0.45
 
 
 @RollPass.zouhar_contact_c2
 def round_oval_c2(self: RollPass):
-    if "round" in self.in_profile.types and "oval" in self.types:
+    if "round" in self.in_profile.classifiers and "oval" in self.classifiers:
         return 0.18
 
 
 @RollPass.zouhar_contact_c1
 def square_oval_c1(self: RollPass):
-    if "square" in self.in_profile.types and "oval" in self.types:
+    if "square" in self.in_profile.classifiers and "oval" in self.classifiers:
         return 0.82
 
 
 @RollPass.zouhar_contact_c2
 def square_oval_c2(self: RollPass):
-    if "square" in self.in_profile.types and "oval" in self.types:
+    if "square" in self.in_profile.classifiers and "oval" in self.classifiers:
         return 0.2
 
 
 @RollPass.zouhar_contact_c3
 def square_oval_c3(self: RollPass):
-    if "square" in self.in_profile.types and "oval" in self.types:
+    if "square" in self.in_profile.classifiers and "oval" in self.classifiers:
         return 1.02
