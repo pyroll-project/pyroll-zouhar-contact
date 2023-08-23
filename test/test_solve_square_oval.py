6995d03fd0a2463e588ef9115965c476c0d83932
Max Weiner <max.weiner@imf.tu-freiberg.de>
2023-08-23 09:59:49 +0200

    add square oval test

---
+++ test/test_solve_square_oval.py
@@ -0,0 +1,55 @@
+import logging
+import webbrowser
+from pathlib import Path
+from pyroll.core import Profile, Roll, RollPass, Transport, RoundGroove, CircularOvalGroove, PassSequence
+
+
+def test_solve_square_oval(tmp_path: Path, caplog):
+    caplog.set_level(logging.DEBUG, logger="pyroll")
+
+    import pyroll.zouhar_contact
+
+    in_profile = Profile.square(
+        side=30e-3,
+        temperature=1200 + 273.15,
+        strain=0,
+        material=["C45", "steel"],
+        flow_stress=100e6
+    )
+
+    sequence = PassSequence(
+        [
+            RollPass(
+                label="Oval I",
+                roll=Roll(
+                    groove=CircularOvalGroove(
+                        depth=8e-3,
+                        r1=6e-3,
+                        r2=40e-3
+                    ),
+                    nominal_radius=160e-3,
+                    rotational_frequency=1
+                ),
+                gap=2e-3,
+            ),
+        ]
+    )
+
+    try:
+        sequence.solve(in_profile)
+    finally:
+        print("\nLog:")
+        print(caplog.text)
+
+    assert sequence[0].has_cached("zouhar_contact_c1")
+
+    try:
+        from pyroll.report import report
+
+        report = report(sequence)
+        f = tmp_path / "report.html"
+        f.write_text(report)
+        webbrowser.open(f.as_uri())
+
+    except ImportError:
+        pass
